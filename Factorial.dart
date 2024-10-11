e3067d1bad9aa91244e0fcc913c958ba05d1e35c
Arifin-only <arifin180104@gmail.com>
2024-10-11 15:48:47 +0800

    Create Factorial.dart

---
+++ Factorial.dart
@@ -0,0 +1,19 @@
+int factorialLoop (int value) {
+
+    var result = 1;
+    
+    for(var i = 1; i <= value; i++){
+    
+    result *= i;
+
+    }
+    
+    return result;
+
+}
+
+void main() {
+
+    print(factorialLoop (10));
+
+}
